acbcdb71c26653007055ff7d7e94c9ac0a507dd2
FZoltan <farkaszoltan28@gmail.com>
2026-09-17 09:02:05 +0200

    GetAllBloggers endpoint is ready to use.

--- BlogApi/Program.cs
+++ BlogApi/Program.cs
@@ -4,7 +4,7 @@ using Scalar.AspNetCore;
 namespace BlogApi
 {
     public class Program
-    { 0
+    { 
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26442cc79c69ae71603e8b9f1e0084f3a743b161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263544c0e1355d388109d6f3cef9ac62d84ea003")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
